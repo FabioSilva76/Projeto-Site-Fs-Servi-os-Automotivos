735c04e01f16f387f6dfb28e886225e0195ab7bb
Fabio S. Silva <fabio_silva76@outlook.com>
2022-04-09 21:04:52 -0300

    testando orçamento

--- Site Fs Serviços/_javaScript/orçamento.js
+++ Site Fs Serviços/_javaScript/orçamento.js
@@ -1,5 +1,21 @@
-var polipint = document.getElementById('polipint');
-function polipintura(){
 
-    console.log('polipint');
-}
+var checkbox = document.querySelectorAll('.checkbox');
+
+var selecionados = 0;
+
+var btn = document.querySelector('#orcar')
+
+btn.addEventListener('click', function(e){
+    e.preventDefault();
+
+    selecionados = 0
+    
+    checkbox.forEach(function(el){
+        if(el.checked){
+            selecionados++;
+        }
+        
+    });
+    console.log(selecionados);
+
+});
