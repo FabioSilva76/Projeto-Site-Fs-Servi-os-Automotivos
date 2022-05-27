7ce26b39b1197918f9ed0d74146aae8bb5f89c8e
Fabio S. Silva <fabio_silva76@outlook.com>
2022-05-27 09:51:44 -0300

    criando checkbox

--- Site Fs Serviços/_javaScript/orçamento.js
+++ Site Fs Serviços/_javaScript/orçamento.js
@@ -1,16 +1,22 @@
+
 var servicos = ['Polimento de Faróis', 'Higienização Interna', 'Lavagem dos Bancos', 'Hidratação em Couro'];
+/* 
+Acima criamos vetores com todas as opções disponiveis para a seleção na mesma ordem que está 
+no formulário. Isto é necessário para evitar usar acentuação no atributo value, do checkbox  */ 
 
+document.addEventListener("DOMContentLoaded",function(){ //Quando o documento for carregado
+    document.getElementById("btnOrcar").addEventListener("click",function(){ // quando houver o click do botão.
+        var elementos= document.getElementsByClassName("servicos"); // Gravamos na vareável todos elementos que contém a class servicos.
 
-document.addEventListener("DOMContentLoaded",function(){
-    document.getElementById("orcar").addEventListener("click",function(){
-        var elementos= document.getElementsByClassName("servicos");
-
-        for(i=0;i<elementos.length;i++){
-            if(elementos[i].checked){''
+        for(i=0;i<elementos.length;i++){ // For começando em 0 e vai até a quantidade de elementos encontrado.
+            if(elementos[i].checked){ // Verificamos se o elemento está com atributo checked ativo. retorna true ou false.
                 document.getElementById("resultado").innerHTML += '<li>'+servicos[i]+'</li>';
+
+                /* Acima inserimos no UL um elemnto LI contendo o valor de vetor na mesma posição que yem a variável [i].
+                Se o segundo checkbox for selecionado, então o valor de [i] é 1 e na posição 1 do vetor é Higienização Interna, a mesma que foi selecionada */
             }
         }
-        document.getElementById("orcar") ;
+        document.getElementById("btnOrcar()");// Desativamos o botão.
     });
 });
 
