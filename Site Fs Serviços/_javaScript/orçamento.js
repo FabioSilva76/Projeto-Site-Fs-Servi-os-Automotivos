aa68ea681d3fbef3835e73f1bb256a2d76f96cbd
Fabio S. Silva <fabio_silva76@outlook.com>
2022-05-26 21:06:08 -0300

    alt2

--- Site Fs Serviços/_javaScript/orçamento.js
+++ Site Fs Serviços/_javaScript/orçamento.js
@@ -1,3 +1,41 @@
+var servicos = ['Polimento de Faróis', 'Higienização Interna', 'Lavagem dos Bancos', 'Hidratação em Couro'];
+
+
+document.addEventListener("DOMContentLoaded",function(){
+    document.getElementById("orcar").addEventListener("click",function(){
+        var elementos= document.getElementsByClassName("servicos");
+
+        for(i=0;i<elementos.length;i++){
+            if(elementos[i].checked){''
+                document.getElementById("resultado").innerHTML += '<li>'+servicos[i]+'</li>';
+            }
+        }
+        document.getElementById("orcar") ;
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /*
 var checkbox = document.querySelectorAll('input');
 
